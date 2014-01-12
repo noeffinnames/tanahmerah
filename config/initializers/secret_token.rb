586a00fece576b30f2b39c9c1c9ea7fbc3094193
Ubuntu <ubuntu@ip-10-185-10-143.ec2.internal>
2014-01-12 00:27:04 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tanahmerah::Application.config.secret_key_base = '170be6d7464bcd98f6ac34ea5bed97ba106226e9b530ed0a4f4494499f2c3681a1d34a47f0ad3ab5047ee055de111b365ef83ca87f14d9b058daddf4fb8a9f70'
