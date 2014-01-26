d5903cd0c58c8a3f839082b251ddf94be469e688
Ubuntu <ubuntu@ip-10-185-10-143.ec2.internal>
2014-01-26 20:27:36 +0000

    added partials

--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -2,10 +2,16 @@ require 'spec_helper'
 
 describe Expense do
 
-  it "raises an error when saving with no incurred date" do
+  it 'raises an error when saving with no incurred date' do
     expense = Expense.new(incurred_date: nil)
     expect{ expense.save! }.to raise_error(ActiveRecord::RecordInvalid, "expense is not valid")
   end
+
+  it 'raises an error when saving with no amount'
+
+  it 'raises an error when saving with no category'
+
+  it 'raises an error when saving with no remarks'
 
 
 end
