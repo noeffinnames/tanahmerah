e3c691dd6418f08724f3e62d12699cc32a67d00a
Ubuntu <ubuntu@ip-10-147-222-73.ec2.internal>
2014-04-27 12:00:26 +0000

    using foreman

--- Rakefile
+++ Rakefile
@@ -2,6 +2,6 @@
 # for example lib/tasks/capistrano.rake, and they will automatically be available to Rake.
 
 require File.expand_path('../config/application', __FILE__)
-require 'rake/dsl_definition'
+#require 'rake/dsl_definition' #TODO - spurious - added this to solve ENV issue
 
 Tanahmerah::Application.load_tasks
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ require 'omniauth-facebook'
 #config.omniauth :facebook, FACEBOOK_KEY, FACEBOOK_SECRET
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, FACEBOOK_KEY, FACEBOOK_SECRET, :auth_type => 'reauthenticate'
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], :auth_type => 'reauthenticate'
 end
