f0162ed866f83a5f02754b2432cb52f6d876fc59
Ubuntu <ubuntu@ip-10-147-222-73.ec2.internal>
2014-04-28 12:01:17 +0000

    seed Chris Smyth as shareholder

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 #User.create(name: 'Chris Smyth', shareholder: true, shareholding_percent: 20, created_at: Time.now)
+my_class = User.find_by_name('Chris Smyth')
+
+my_class.update_attributes(
+   :shareholder => true
+)
